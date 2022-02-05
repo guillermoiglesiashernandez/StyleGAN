9ba3a3d48c183e3013b8dee945ed9e09d59c6196
guillermoiglesiashernandez <guillermoiglesiashernandez@alumnos.upm.es>
2022-02-05 15:03:09 +0100

    Solved library dependency

--- train/train.py
+++ train/train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from train.style_gan import StyleGAN
 from train.plot_images import plot_images
